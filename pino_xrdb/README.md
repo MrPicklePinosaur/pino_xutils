eb398e0180ac2604f9a613acaa3926479c1cd096
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-08-25 17:13:37 -0400

    xrdb struct

---
+++ pino_xrdb/README.md
@@ -0,0 +1,5 @@
+
+# pino_xrdb
+
+simple parser for `xrdb` command. requires `xrdb` to be installed.
+
--- pino_xrdb/src/lib.rs
+++ pino_xrdb/src/lib.rs
@@ -1,4 +1,5 @@
 
+use std::collections::HashMap;
 use std::process::Command;
 use thiserror::Error;
 use anyhow::{Result, Error};
@@ -9,6 +10,57 @@ pub enum XrdbError {
     XrdbMissing,
     #[error("xrdb exited with error: {0}")]
     XrdbErrored(String),
+}
+
+pub struct Xrdb {
+    db: HashMap<String, HashMap<String, String>>,
+    univeral: HashMap<String, String> 
+}
+
+impl Xrdb {
+
+    // pub fn new() -> Result<Self> {
+         
+    // }
+
+    /// Insert a new resource
+    pub fn insert(&mut self, program: &str, res: String, val: String) {
+        self.get_prog_mut(program).insert(res, val);
+    }
+
+    /// Insert a universal resource
+    pub fn insert_univeral(&mut self, res: String, val: String) {
+        self.univeral.insert(res, val);
+    }
+
+    /// Query a given resource
+    pub fn query(&self, program: &str, res: &str) -> Option<String> {
+        if let Some(prog) = self.db.get(program) {
+            if let Some(val) = prog.get(res) {
+                return Some(val.to_owned());
+            }
+        }
+        
+        // check if resource was defined in universal
+        self.univeral.get(res).map(|v| v.to_owned())
+    }
+
+    /// Return reference to query table or creates it if not exist
+    fn get_prog(&mut self, program: &str) -> &HashMap<String, String> {
+        if !self.db.contains_key(program) {
+            self.db.insert(program.to_owned(), HashMap::new());
+        }
+        self.db.get(program).unwrap()
+    }
+
+    /// Mutable version of [get_prog]
+    fn get_prog_mut(&mut self, program: &str) -> &mut HashMap<String, String> {
+        if !self.db.contains_key(program) {
+            self.db.insert(program.to_owned(), HashMap::new());
+        }
+        self.db.get_mut(program).unwrap()
+    }
+
 }
 
 pub fn xrdb() -> Result<String> {
@@ -26,12 +78,19 @@ pub fn xrdb() -> Result<String> {
     Ok(output_str)
 }
 
+pub fn parse_xrdb(input: &str) {
+    
+    for line in input.lines() {
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::xrdb;
+    use crate::*;
 
     #[test]
     fn command(){
-        println!("{}", xrdb().unwrap());
+        let output = xrdb().unwrap();
+        parse_xrdb(&output);
     }
 }
